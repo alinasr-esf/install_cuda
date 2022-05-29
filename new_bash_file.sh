0ffb01ade9d31d8d39d4acc43e9bbfcc0101fa87
alinasr-esf <75745835+alinasr-esf@users.noreply.github.com>
2022-05-29 16:45:54 +0430

    Update new_bash_file.sh

--- new_bash_file.sh
+++ new_bash_file.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+#installing base PKG
 #sudo apt update
-#sudo apt -y install curl 
+#sudo apt install curl -y
 #sudo apt -y install git 
 #sudo apt -y install figlet
-#sudo apt update -y
-#sudo apt upgrade -y
+#sudo apt update
+#sudo apt upgrade 
+
+#installing cuda toolkit
 #wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 #sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 #wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
@@ -13,8 +17,8 @@
 #sudo apt-get update
 #sudo apt-get -y install cuda
 
+#installing ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl -y # if you haven't already installed curl 
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update
 #sudo apt-get install ros-kinetic-desktop-full -y
@@ -25,3 +29,4 @@ source ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
 catkin_make
+
