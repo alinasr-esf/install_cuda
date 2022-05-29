965202423265715e04a9f061e7cc4272454595f3
alinasr-esf <75745835+alinasr-esf@users.noreply.github.com>
2022-05-29 12:36:10 +0430

    Update new_bash_file.sh

--- new_bash_file.sh
+++ new_bash_file.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+echo "update"
 sudo apt update
+echo "try to install curl"
 sudo apt -y install curl 
+echo "try to install git
 sudo apt -y install git 
+echo "try to install figlet
 sudo apt -y install figlet
-sudo apt update
-sudo apt upgrade
+sudo apt update -y
+sudo apt upgrade -y
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 #wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
