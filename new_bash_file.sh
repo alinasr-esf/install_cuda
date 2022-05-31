6c74ebab0d0ac71a883b6ba19bba877126458948
alinasr-esf <75745835+alinasr-esf@users.noreply.github.com>
2022-05-31 11:49:53 +0430

    finilization installing normal PKG & ROS

--- new_bash_file.sh
+++ new_bash_file.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 #installing base PKG
-#sudo apt update
-#sudo apt install curl -y
-#sudo apt -y install git 
-#sudo apt -y install figlet
-#sudo apt update
-#sudo apt upgrade 
+sudo apt update
+sudo apt install curl -y
+sudo apt -y install git 
+sudo apt -y install figlet
+sudo apt update
+sudo apt upgrade 
 
 #installing cuda toolkit
-#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-#sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-#sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.1-510.47.03-1_amd64.deb
-#sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
-#sudo apt-get update
-#sudo apt-get -y install cuda
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.1/local_installers/cuda-repo-ubuntu2004-11-6-local_11.6.1-510.47.03-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-6-local_11.6.1-510.47.03-1_amd64.deb
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
 
 #installing ROS
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -29,4 +29,3 @@ source ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/
 catkin_make
-
