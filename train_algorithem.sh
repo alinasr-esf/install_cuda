94b466e5967529fd3fcdefc3cf9d5cff40b1e388
alinasr-esf <75745835+alinasr-esf@users.noreply.github.com>
2022-06-06 08:40:03 +0430

    Update train_algorithem.sh

--- train_algorithem.sh
+++ train_algorithem.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo apt update
 sudo apt-get install python3-pip python3-dev python3-virtualenv -y
-virtualenv --system-site-packages ~/YoloV5_train
+#virtualenv --system-site-packages ~/YoloV5_train
 source ~/YoloV5_train/bin/activate
 cd YoloV5_train
-git clone https://github.com/ultralytics/yolov5
+#git clone https://github.com/ultralytics/yolov5
 cd yolov5
 pip install -r requirements.txt
 wget https://drive.google.com/file/d/1nArFobMVGVFR-f6x2BoR0vw9q2LVEWNX/view?usp=sharing
