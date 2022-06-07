634653a0e58ba39c0613efbf4af16eb2306a7490
alinasr-esf <75745835+alinasr-esf@users.noreply.github.com>
2022-06-07 09:00:23 +0430

    change some update

--- train_algorithem.sh
+++ train_algorithem.sh
@@ -3,13 +3,14 @@ sudo apt update
 sudo apt-get install python3-pip python3-dev python3-virtualenv -y
 sudo apt-get install lm-sensors -y
 sudo apt install htop -y
-virtualenv --system-site-packages ~/YoloV5_train
+sudo apt install git -y
+#virtualenv --system-site-packages ~/YoloV5_train
 source ~/YoloV5_train/bin/activate
 cd YoloV5_train
 git clone https://github.com/ultralytics/yolov5
 cd yolov5
 pip install -r requirements.txt
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?id=1nArFobMVGVFR-f6x2BoR0vw9q2LVEWNX&export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?id=1nArFobMVGVFR-f6x2BoR0vw9q2LVEWNX&export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O dataset.zip && rm -rf /tmp/cookies.txt
-unzip dataset.zip
+#wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?id=1nArFobMVGVFR-f6x2BoR0vw9q2LVEWNX&export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?id=1nArFobMVGVFR-f6x2BoR0vw9q2LVEWNX&export=download&id=FILEID' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=FILEID" -O dataset.zip && rm -rf /tmp/cookies.txt
+#unzip dataset.zip
 sudo apt update
 sudo apt-get upgrade -y
