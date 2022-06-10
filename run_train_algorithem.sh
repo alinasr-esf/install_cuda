5a837c1b87ffe49f16c7cb777c68e86964ad3556
alinasr-esf <75745835+alinasr-esf@users.noreply.github.com>
2022-06-09 23:06:32 -0700

    Update run_train_algorithem.sh

--- run_train_algorithem.sh
+++ run_train_algorithem.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/YoloV5_train/bin/activate
-cd ~/YoloV5_train/yolov5
-python3 train.py --img 640 --batch 64 --epochs 30 --data bottle_glass.yaml  --name first_train --weights yolov5l.pt
+cd ~/yolov5
+python3 train.py --img 640 --batch 32 --epochs 300 --data bottle_glass.yaml  --name secound_train_300epoch --weights yolov5l.pt
